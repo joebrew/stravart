8f1547b7e18a540d9067ebda22a9d19abda20a63
Joe Brew <joebrew@gmail.com>
2020-01-07 13:57:03 +0100

    updated

--- app.R
+++ app.R
@@ -1,7 +1,5 @@
 library(shiny)
 source('global.R')
-source('plot_functions.R')
-source('utils.R')
 
 # Read in credentials
 creds <- yaml::yaml.load_file('credentials.yaml')
@@ -42,7 +40,7 @@ uiFunc <- function(req) {
     # OAuthorize
     # url <- oauth2.0_authorize_url(api, app, scope=scope, query_extra=params)
     # redirect <- sprintf("location.replace(\"%s\");", url)
-    uiOutput('auth_submit_button')
+    uiOutput('auth_submit')
     # tags$script(HTML(redirect))
   } else {
     # Manually create a token
@@ -64,11 +62,19 @@ server <- function(input, output, session) {
   })
   
   # Button for going to authentication page
-  output$auth_submit_button <- renderUI({
+  output$auth_submit <- renderUI({
     url <- oauth2.0_authorize_url(api, app, scope=scope, query_extra=params)
-    a(
-      img(src = 'btn_strava_connectwith_light.png'),
-      href = url
+    
+    fluidPage(
+      fluidRow(column(12, align = 'center', h1('GPSart'))),
+      fluidRow(
+        column(12, align = 'center',
+               a(
+                 img(src = 'btn_strava_connectwith_light.png'),
+                 href = url
+               )
+        )
+      )
     )
   })
   
@@ -85,10 +91,130 @@ server <- function(input, output, session) {
     cache = FALSE
   )
   
-  resp <- get_athlete(stoken = token)
+  # Reactive values for holding data
+  session_data <- reactiveValues()
+  
+  # Get athlete data
+  withProgress({
+    shiny::setProgress(value=0.05,message = 'Logged in')
+    
+    # Retrieve the athlete
+    message('Getting athlete...')
+    my_athlete <- get_athlete(stoken = token)
+    id <- my_athlete$id
+    message('---Got athlete: ', my_athlete$username, ': ', my_athlete$firstname, ' ', my_athlete$lastname,
+            ' (id: ', id, ')')
+    
+    # Save token
+    cache_dir <- paste0('./cache/', id)
+    dir.create(cache_dir,showWarnings = FALSE)
+    saveRDS(token, paste0(cache_dir, '/token.rds'))
+    
+    shiny::setProgress(value=0.2,message = 'Connecting to Strava data')
+    
+    # Fetch activities, but first see if that athlete already has some activities in the database
+    # (fetch fewer if the athlete already has some)
+    ids_already_in_db <- 
+      dbGetQuery(conn = con,
+                 statement = paste0('select id from activities where activities."athlete.id" = ',
+                                    "'",
+                                    id,
+                                    "'")) %>% .$id
+    athlete_ids_already_in_db <- 
+      dbGetQuery(conn = con,
+                 statement = paste0('select id from athletes')) %>% .$id
+    activities_column_names <- 
+      names(dbGetQuery(conn = con,
+                       statement = paste0('select * from activities limit 1'))[0,])
+    # Get fewer activities if the id is already in the db
+    # (this is just for the sake of speed, and should be improved at some point so as to ensure we're not over/under-fetching)
+    shiny::setProgress(value=0.25,message = 'Downloading activity data')
+    if(id %in% athlete_ids_already_in_db){
+      message('Athlete already in DB. Just fetching some data')
+      my_acts <- get_activity_list_by_page(token,30,1)
+    } else {
+      message('New athlete. Fetching lots of data')
+      my_acts <- get_activity_list_by_page(token,200,100)
+    }
+    shiny::setProgress(value=0.35,message = 'Done downloading activity data')
+    
+    my_acts.df <- my_acts %>% 
+      tidy_activities()
+    # Remove those that are already in the database
+    new_acts <- my_acts.df %>%
+      filter(!id %in% ids_already_in_db)
+    # Add new_acts to db if there are any
+    if(nrow(new_acts) > 0){
+      # Remove excess columns
+      new_acts <- data.frame(new_acts)
+      new_acts <- new_acts[,names(new_acts) %in%activities_column_names]
+      message('---Writing ', nrow(new_acts), ' new activities to the database for athlete ', id)
+      # Write the new acts to the table
+      dbWriteTable(conn = con,
+                   name = 'activities',
+                   value = new_acts,
+                   append = TRUE,
+                   row.names = FALSE)
+    } else {
+      message('---No new activities for athlete ', id)
+    }
+    # Retrieve the full activities for this athlete from the table
+    activities_in_db <- 
+      dbGetQuery(conn = con,
+                 statement = paste0('select * from activities where activities."athlete.id" = ',
+                                    "'",
+                                    id,
+                                    "'"))
+    
+    # Athlete info
+    # If this athlete is already in the db, drop the  and overwrite
+    if(id %in% athlete_ids_already_in_db){
+      # Delete
+      dbSendQuery(conn = con,
+                  statement = paste0('DELETE FROM athletes WHERE id = ', id))
+      # Add new row
+      dbWriteTable(conn = con,
+                   name = 'athletes', 
+                   value = data.frame(tidy_athlete(my_athlete)),
+                   append = TRUE,
+                   row.names = FALSE)
+      
+    }
+
+    # GEOCODING
+    shiny::setProgress(value=0.6,message = 'Extracting starting locations for reverse geocoding')
+    # Get starting location ids
+    slids <- generate_starting_location_id(activities_in_db)
+    slids <- slids[!is.na(slids)]
+    slids <- sort(unique(slids))
+    # Get already geocoded starting locations
+    old_starting_locations <- dbReadTable(conn = con,name = 'starting_locations')
+    # Remove those slids which are already geocoded
+    slids <- slids[!slids %in% old_starting_locations$starting_location_id]
+    shiny::setProgress(value=0.7,message = 'Reverse geocoding activity starting locations')
+    if(length(slids) > 0){
+      # Geocode them
+      geocoded_slids <- reverse_geocode(slids = slids)
+      # Add the geocoded slids to the db
+      message('Adding newly geocoded starting locations to the database')
+      dbWriteTable(con, "starting_locations", data.frame(geocoded_slids), append = TRUE, row.names = FALSE)
+    }
+    
+    # Upodate the in-session data specific to this athlete
+    session_data$athlete <- my_athlete
+    session_data$activities <- activities_in_db
+    session_data$starting_locations <- dbReadTable(conn = con, 
+                                                   name = 'starting_locations')
+    
+    shiny::setProgress(value=1,message = 'Complete')
+    
+  },
+  min = 0,
+  max = 1)
 
   output$code <- renderText({
-    resp$lastname
+    sda <- session_data$athlete
+    sda$lastname
   })
 }
 onStop(function() {
--- global.R
+++ global.R
@@ -14,7 +14,6 @@ library(highcharter)
 library(shinyjs)
 library(shinydashboard)
 library(shinythemes)
-library(logging)
 library(googlePolylines)
 library(tidyquant)
 library(yaml)
@@ -29,44 +28,12 @@ library(sp)
 # Get creds
 creds <- yaml.load_file('credentials.yaml')
 
-# initialise logging ----
-# basicConfig()
-# dir.create('./logs/',showWarnings = F)
-# addHandler(writeToFile,file=glue('./logs/{strftime(Sys.time(),\'%Y%m%d-%H%M%S\')}.log'))
-#addHandler(writeToConsole)
-
-# file dependencies ----
-
-# loginfo('Load file depencencies',logger='authentication')
+# Source helper scripts
 source('./utils.R')
 source("./dplyr_verbs.R")
-
-
-# Application options ----
-
-# dir.create('cache',showWarnings = F)
-# cache <- F # whether to load cached data (must have authenticated before)
-# 
-# if (cache) {
-#   loginfo('Use cached credentials and data',logger='authentication')
-# }
-
-# app misc ----
-periods <- list(
-  'This week' = c(floor_date(Sys.Date()-1,unit = 'week')+1,as.Date(strftime(Sys.Date(),'%Y-%m-%d'))),
-  'Last 7 days' = c(Sys.Date()-6,Sys.Date()),
-  'This month' = c(floor_date(Sys.Date(),unit = 'month'),as.Date(strftime(Sys.Date(),'%Y-%m-%d'))),
-  'Last 30 days' = c(Sys.Date()-29,Sys.Date()),
-  'This year' = c(floor_date(Sys.Date(),unit = 'year'),Sys.Date()),
-  'Last week' = c(floor_date(Sys.Date()-1-7,unit = 'week')+1,floor_date(Sys.Date()-1,unit = 'week')),
-  'Last month' = c(floor_date(Sys.Date(),unit = 'month') - months(1),floor_date(Sys.Date(),'month')-1),
-  'All time' = c(as.Date('1900-01-01'),as.Date(Sys.Date())),
-  'Custom' = c(floor_date(Sys.Date()-1,unit = 'week')+1,as.Date(strftime(Sys.Date(),'%Y-%m-%d')))
-)
-
+source('./plot_functions.R')
 
 # Connect to the database
-creds <- yaml.load_file('credentials.yaml')
 creds_list <- credentials_extract( credentials_path = '.')
 creds_list <- creds_list[names(creds_list) %in% c('dbname', 'host', 'port', 'user', 'password')]
 creds_list$drv <- DBI::dbDriver("PostgreSQL")
@@ -80,6 +47,3 @@ for(i in 1:length(creds_list)){
     message('---', names(creds_list)[i], ': ', creds_list[[i]])
   }
 }
-# 
-# # Already geocoded starting locations
-# old_starting_locations <- dbReadTable(conn = con,name = 'starting_locations')
